a001c72f4e69815927ea0e9dc20294b4e2087add
“soya” <2820116913@qq.com>
2023-04-27 17:18:11 +0800

    线程加锁 静态的 非静态的

---
+++ Javaoop/src/shz/soya/threadTest/SaleTicketDemo4.java
@@ -0,0 +1,38 @@
+package shz.soya.threadTest;
+
+/**
+ * @author soya
+ * @version 1.0
+ * @project javaNovice
+ * @description 非静态的锁
+ * @date 2023/4/27 17:00:06
+ */
+public class SaleTicketDemo4 {
+    public static void main(String[] args) {
+        TicketSaleRunnable tr = new TicketSaleRunnable();
+        Thread t1 = new Thread(tr, "窗口一");
+        Thread t2 = new Thread(tr, "窗口二");
+        Thread t3 = new Thread(tr, "窗口三");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class TicketSaleRunnable implements Runnable {
+    private int ticket = 100;
+
+    public void run() {//直接锁这里，肯定不行，会导致，只有一个窗口卖票
+        while (ticket > 0) {
+            saleOneTicket();
+        }
+    }
+
+    public synchronized void saleOneTicket() {//锁对象是this，这里就是TicketSaleRunnable对象，因为上面3个线程使用同一个TicketSaleRunnable对象，所以可以
+        if (ticket > 0) {//不加条件，相当于条件判断没有进入锁管控，线程安全问题就没有解决
+            System.out.println(Thread.currentThread().getName() + "卖出一张票，票号:" + ticket);
+            ticket--;
+        }
+    }
+}
