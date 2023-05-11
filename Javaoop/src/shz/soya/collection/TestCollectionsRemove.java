5129ff8a714abc4c3de70e3c0a30efc6d3a2fb20
“soya” <2820116913@qq.com>
2023-05-11 17:36:19 +0800

    removeAll 与 retainAll

---
+++ Javaoop/src/shz/soya/collection/TestCollectionsRemove.java
@@ -0,0 +1,73 @@
+package shz.soya.collection;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.function.Predicate;
+
+/**
+ * @author soya
+ * @version 1.0
+ * @project javaNovice
+ * @description collections remove
+ * @date 2023/5/11 17:31:27
+ */
+public class TestCollectionsRemove {
+    @Test
+    public void test01(){
+        Collection coll = new ArrayList();
+        coll.add("小李广");
+        coll.add("扫地僧");
+        coll.add("石破天");
+        coll.add("佛地魔");
+        System.out.println("coll = " + coll);
+
+        coll.remove("小李广");
+        System.out.println("删除元素\"小李广\"之后coll = " + coll);
+
+        coll.clear();
+        System.out.println("coll清空之后，coll = " + coll);
+    }
+
+    @Test
+    public void test02() {
+        Collection coll = new ArrayList();
+        coll.add("小李广");
+        coll.add("扫地僧");
+        coll.add("石破天");
+        coll.add("佛地魔");
+        System.out.println("coll = " + coll);
+
+        Collection other = new ArrayList();
+        other.add("小李广");
+        other.add("扫地僧");
+        other.add("尚硅谷");
+        System.out.println("other = " + other);
+
+        coll.removeAll(other);
+        System.out.println("coll.removeAll(other)之后，coll = " + coll);
+        System.out.println("coll.removeAll(other)之后，other = " + other);
+    }
+
+    @Test
+    public void test03() {
+        Collection coll = new ArrayList();
+        coll.add("小李广");
+        coll.add("扫地僧");
+        coll.add("石破天");
+        coll.add("佛地魔");
+        System.out.println("coll = " + coll);
+
+        Collection other = new ArrayList();
+        other.add("小李广");
+        other.add("扫地僧");
+        other.add("尚硅谷");
+        System.out.println("other = " + other);
+
+        coll.retainAll(other);
+        System.out.println("coll.retainAll(other)之后，coll = " + coll);
+        System.out.println("coll.retainAll(other)之后，other = " + other);
+    }
+}
