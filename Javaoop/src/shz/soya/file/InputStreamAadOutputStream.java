e00a2cfd52b1fa8ba01ebdb23fe179cd2bff7e53
soya <2820116913@qq.com>
2023-05-18 15:41:51 +0800

    连接测试

---
+++ Javaoop/src/shz/soya/file/InputStreamAadOutputStream.java
@@ -0,0 +1,17 @@
+package shz.soya.file;
+
+/**
+ * @author soya
+ * @version 1.0
+ * @project javaNovice
+ * @description input and output
+ * @date 2023/5/17 22:52:38
+ */
+public class InputStreamAadOutputStream {
+    public static void main(String[] args) {
+
+    }
+    public static void inputTest1(){
+
+    }
+}
